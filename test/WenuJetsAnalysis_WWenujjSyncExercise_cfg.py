5f70692287e4fee0b14b646128913f8d872d5e81
Kalanand Mishra <sha1-f871bac3565530a83b07ed2234745ffb0c9fc5a4@cern.ch>
2011-04-16 23:10:24 +0000

    update BuildFile.xml

--- test/WenuJetsAnalysis_WWenujjSyncExercise_cfg.py
+++ test/WenuJetsAnalysis_WWenujjSyncExercise_cfg.py
@@ -27,7 +27,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 if not isMC:
     process.GlobalTag.globaltag = 'GR_R_39X_V5::All'
 else:
-    process.GlobalTag.globaltag = 'START39_V8::All'
+    process.GlobalTag.globaltag = 'START311_V2A::All'
 
 OutputFileName = "demo.root"
 numEventsToRun = -1
@@ -42,11 +42,32 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
-      '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/454/ACDEDA3C-B7D3-DF11-A7A1-0030487C6A66.root',
-      '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/454/223CD93D-B7D3-DF11-885E-0030487CD7B4.root',
-      '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/453/EAB3E588-B6D3-DF11-8BDC-0030487A3232.root',
-      '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/453/AA0C5537-B7D3-DF11-9194-0030487CD7C6.root',
-
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0026/FE40206F-2453-E011-A011-003048F0E1EE.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0026/D62114D9-F952-E011-A486-003048F0E3AE.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0026/6A4D222D-EC52-E011-8021-003048F0E5A2.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0026/1C119FC9-EE52-E011-86CB-0030487F938B.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/F4AE4A60-E652-E011-B495-0030487F92FF.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/C40F587B-E752-E011-A002-0030487F9351.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/BA617B42-E752-E011-8F4B-0030487F9307.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/B80E704E-E652-E011-A6FC-0030487FA629.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/90AC4971-E752-E011-881A-0030487F932D.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/78C05C44-E752-E011-95E5-0030487F9307.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/50E94B31-E752-E011-8FD3-0030487F91DB.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/50CEAD5E-E652-E011-8BC5-0030487F92FF.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/30551D41-E752-E011-AA16-0030487F929B.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/2C6C1D71-E552-E011-B975-002481E945C4.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/1E1D17C5-E552-E011-880C-003048D4DFA8.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/083A7E32-E752-E011-9C7B-0030487F91DB.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0025/069C7D32-E752-E011-B3BD-0030487F91DB.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0024/EAA84FDA-C752-E011-9119-0030487E52A5.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0024/64F298DB-DC52-E011-B579-0030487F9367.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0024/5CF7BEDB-DC52-E011-8612-0030487F933D.root',
+       '/store/mc/Spring11/GluGluToHToWWToLNuQQ_M-170_7TeV-powheg-pythia6/AODSIM/PU_S1_START311_V1G1-v1/0024/527222CB-DC52-E011-A990-0030487FA625.root'
+    
+##       '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/454/ACDEDA3C-B7D3-DF11-A7A1-0030487C6A66.root',
+##       '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/454/223CD93D-B7D3-DF11-885E-0030487CD7B4.root',
+##       '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/453/EAB3E588-B6D3-DF11-8BDC-0030487A3232.root',
+##       '/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/453/AA0C5537-B7D3-DF11-9194-0030487CD7C6.root',
 ##        '/store/mc/Winter10/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/AODSIM/E7TeV_ProbDist_2011Flat_BX156_START39_V8-v1/0062/FE9CF7A8-5A2D-E011-9775-0030486790B0.root',
 ##               '/store/mc/Winter10/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/AODSIM/E7TeV_ProbDist_2011Flat_BX156_START39_V8-v1/0062/F2EDDFEA-592D-E011-97B7-0030486791F2.root',
 ##               '/store/mc/Winter10/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/AODSIM/E7TeV_ProbDist_2011Flat_BX156_START39_V8-v1/0062/F0ECB760-5A2D-E011-8CF0-002618943811.root',
