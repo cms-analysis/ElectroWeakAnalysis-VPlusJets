dcaedf480ffbbe8f6da29e7730285a8aca85f20a
Nhan Viet Tran <sha1-8a19d334a4648fb26ba9e7879a577ce46f3df51a@cern.ch>
2011-11-14 15:16:49 +0000

    Update, close the ROOT file

--- interface/QGLikelihoodCalculator.h
+++ interface/QGLikelihoodCalculator.h
@@ -19,7 +19,9 @@ class QGLikelihoodCalculator {
  public:
 
   QGLikelihoodCalculator( const std::string& fileName="QG_QCD_Pt_15to3000_TuneZ2_Flat_7TeV_pythia6_Spring11-PU_S1_START311_V1G1-v1.root", unsigned nPtBins=20, unsigned int nRhoBins=17 );
-  virtual ~QGLikelihoodCalculator() {};
+  virtual ~QGLikelihoodCalculator() {
+		histoFile_->Close(); 
+	};
 
   float computeQGLikelihood( float pt, int nCharged, int nNeutral, float ptD, float rmsCand=-1. );
   float computeQGLikelihoodPU( float pt, float rhoPF, int nCharged, int nNeutral, float ptD, float rmsCand=-1. );
