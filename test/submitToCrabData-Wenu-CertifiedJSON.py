393c981368b644eeae7d115ed41ad1da85856c96
Fan Yang <sha1-89248e455302f8b21edd4e6b7025364acd9760a8@cern.ch>
2011-12-14 19:09:14 +0000

    Submit jobs script for 2010+2011A+B

--- test/submitToCrabData-Wenu-CertifiedJSON.py
+++ test/submitToCrabData-Wenu-CertifiedJSON.py
@@ -2,80 +2,105 @@ import os,sys
 import string, re
 from time import gmtime, localtime, strftime
 
-##------ Please set ONLY one of the four flags to True -------
 physMode   = "WenuJets_"
 ConfigFile = "WenuJetsAnalysis_cfg.py"
+DefTrig    = "'HLT_Photon15_Cleaned_L1R','HLT_Ele15_*','HLT_Ele17_*','HLT_Ele22_*','HLT_Ele25_*','HLT_Ele27_*','HLT_Ele32_*'"
 
-## ----- 2010A
-#dataset    =     ["/EG/Run2010A-Apr21ReReco-v1/AOD"]
-#channels   =         ["Run2010A-Apr21ReReco-v1"]
-#outputdir  = "CMSSW428-Run2010A-Apr21ReReco-v1"
-#RunRange   =     ["136033-149442"]
-#JSON       = "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt"
+dataset    = [
+    "/EG/Run2010A-Apr21ReReco-v1/AOD",
+    "/Electron/Run2010B-Apr21ReReco-v1/AOD",
+    "/SingleElectron/Run2011A-May10ReReco-v1/AOD",
+    "/ElectronHad/Run2011A-PromptReco-v4/AOD",
+    "/ElectronHad/Run2011A-PromptReco-v4/AOD",
+    "/ElectronHad/Run2011A-05Aug2011-v1/AOD",
+    "/ElectronHad/Run2011A-05Aug2011-v1/AOD",
+    "/ElectronHad/Run2011A-PromptReco-v6/AOD",
+    "/ElectronHad/Run2011A-PromptReco-v6/AOD",
+    "/ElectronHad/Run2011B-PromptReco-v1/AOD",
+    "/ElectronHad/Run2011B-PromptReco-v1/AOD",
+    "/ElectronHad/Run2011B-PromptReco-v1/AOD",
+    "/ElectronHad/Run2011B-PromptReco-v1/AOD"]
+channels   = [
+    "EG-Run2010A-Apr21ReReco-v1-trigv2010",
+    "Electron-Run2010B-Apr21ReReco-v1-trigv2010",
+    "SingleElectron-Run2011A-May10ReReco-v1-trig5E32",
+    "ElectronHad-Run2011A-PromptReco-v4-trig1E33",
+    "ElectronHad-Run2011A-PromptReco-v4-trig1p4E33",
+    "ElectronHad-Run2011A-05Aug2011-v1-trigv2E33v11",
+    "ElectronHad-Run2011A-05Aug2011-v1-trigv2E33v12",
+    "ElectronHad-Run2011A-PromptReco-v6-trigv2E33v12",
+    "ElectronHad-Run2011A-PromptReco-v6-trigv3E33v00",
+    "ElectronHad-Run2011B-PromptReco-v1-trigv3E33v20",
+    "ElectronHad-Run2011B-PromptReco-v1-trigv3E33v23",
+    "ElectronHad-Run2011B-PromptReco-v1-trigv5E33v14",
+    "ElectronHad-Run2011B-PromptReco-v1-trigv5E33v22"]
+trigname   = [
+    "'HLT_Ele10_*','HLT_Ele15_*','HLT_Ele17_*'",
+    "'HLT_Ele10_*','HLT_Ele15_*','HLT_Ele17_*'",
+    "'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*'",
+    "'HLT_Ele17_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT15_v*'",
+    "'HLT_Ele22_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v*'",
+    "'HLT_Ele22_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v*'",
+    "'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v*'",
+    "'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v*'",
+    "'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v*'",
+    "'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v2'",
+    "'HLT_Ele30_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_DiCentralJet30_PFMHT25_v1'",
+    "'HLT_Ele27_WP80_DiCentralPFJet25_PFMHT15_v4'",
+    "'HLT_Ele27_WP80_DiCentralPFJet25_PFMHT15_v5'"]
+RunRange   = [
+    "136033-149442",
+    "136033-149442",
+    "160404-163869",
+    "165088-167038",
+    "167039-167913",
+    "170249-170825",
+    "170826-172619",
+    "172620-173235",
+    "173236-173692",
+    "175832-176460",
+    "176461-178419",
+    "178420-179958",
+    "179959-180252"]
+JSON       = [
+    "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt",
+    "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt",
+    "Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt",
+    "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt"]
+condor     = [1,1,1,1,1,1,1,1,1,1,1,1,1] # Total jobs 13 now
 
-
-## ----- 2010B
-#dataset    = ["/Electron/Run2010B-Apr21ReReco-v1/AOD"]
-#channels   =           ["Run2010B-Apr21ReReco-v1"]
-#outputdir  =   "CMSSW428-Run2010B-Apr21ReReco-v1"
-#RunRange   =     ["136033-149442"]
-#JSON       = "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt" 
-
-
-## ----- 2011A May10ReReco
-#dataset    = ["/SingleElectron/Run2011A-May10ReReco-v1/AOD"]
-#channels   =                 ["Run2011A-May10ReReco-v1"]
-#outputdir  =         "CMSSW428-Run2011A-May10ReReco-v1"
-#RunRange   =     ["160404-163869"]
-#JSON       = "Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt" 
-
-
-## ----- 2011A PromptReco-v4
-#dataset    = ["/SingleElectron/Run2011A-PromptReco-v4/AOD"]
-#channels   =                 ["Run2011A-PromptReco-v4"]
-#outputdir  =         "CMSSW428-Run2011A-PromptReco-v4"
-#RunRange   =     ["165071-168437"]
-#JSON       = "Cert_160404-173692_7TeV_PromptReco_Collisions11_JSON.txt" 
-
-
-## ----- 2011A Run2011A-05Aug2011-v1
-#dataset    = ["/SingleElectron/Run2011A-05Aug2011-v1/AOD"]
-#channels   =                 ["Run2011A-05Aug2011-v1"]
-#outputdir  =         "CMSSW428-Run2011A-05Aug2011-v1"
-#RunRange   =     ["170249-172619"]
-#JSON       = "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt" 
-
-
-# ----- 2011A Run2011A-PromptReco-v6
-dataset    = ["/SingleElectron/Run2011A-PromptReco-v6/AOD"]
-channels   =                 ["Run2011A-PromptReco-v6"]
-outputdir  =         "CMSSW428-Run2011A-PromptReco-v6"
-RunRange   =     ["172620-175770"]
-JSON       = "Cert_160404-173692_7TeV_PromptReco_Collisions11_JSON.txt" 
-
+MyResilientArea = "/yangf/CMSSW428/" +physMode
 
 ## ------------------------------------------
-condor     = [1]
-MyResilientArea = "/yangf/CMSSW428/" +physMode + outputdir
-## ------------------------------------------
 
 
-def changeMainConfigFile(trigpath):
+def changeMainConfigFile(trigpath,nowtrigname):
     fin  = open(ConfigFile)
-    pset_cfg      = "py_" + trigpath + ".py"
+    pset_cfg      = physMode + "py_" + trigpath + ".py"
     outfile_root  = physMode + trigpath + ".root"
     fout = open(pset_cfg,"w")
     for line in fin.readlines(): 
         if  line.find("demo.root")!=-1:
             line=line.replace("demo.root",outfile_root)
+        if  line.find(DefTrig)!=-1:
+            line=line.replace(DefTrig,nowtrigname)
         fout.write(line)
     print pset_cfg + " has been written.\n"
 
 
-def changeCrabTemplateFile(outfile, index):
+def changeCrabTemplateFile(outfile, index, nowJSON, nowRang):
     fin  = open("crabTemplate.cfg")
-    pset_cfg      = "py_" + outfile + ".py"
-    pset_crab     = "crabjob_" + outfile + ".cfg"
+    pset_cfg      = physMode + "py_" + outfile + ".py"
+    pset_crab     = physMode + "cb_" + outfile + ".cfg"
     outfile_root  = physMode + outfile + ".root"
     fout = open(pset_crab,"w")
     for line in fin.readlines():
@@ -83,13 +108,13 @@ def changeCrabTemplateFile(outfile, index):
             line=line.replace("mydataset",dataset[index])
             fout.write("\n")
             fout.write("runselection="+RunRange[index]+"\n")
-            fout.write("lumi_mask="+JSON+"\n")
+            fout.write("lumi_mask="+nowJSON+"\n")
         if line.find("myanalysis")!=-1:
             line=line.replace("myanalysis",pset_cfg)    
         if  line.find("myrootfile")!=-1:
             line=line.replace("myrootfile",outfile_root)
         if  line.find("myresilient")!=-1:
-            line=line.replace("myresilient",MyResilientArea)    
+            line=line.replace("myresilient",MyResilientArea+nowRang+"_"+outfile)    
         if line.find("glite")!=-1 and condor[index]!=0:
             line=line.replace("glite", "condor")        
         fout.write(line)        
@@ -98,15 +123,14 @@ def changeCrabTemplateFile(outfile, index):
       
     print pset_crab + " has been written.\n"
 
-
     
 ###################
 for i in range(len(channels)):
-    changeMainConfigFile(channels[i])
-    changeCrabTemplateFile(channels[i],i)
+    changeMainConfigFile(channels[i],trigname[i])
+    changeCrabTemplateFile(channels[i],i,JSON[i],RunRange[i])
 
 for i in range(len(channels)):
     #if i<9: continue
-    submitcommand = "crab -create -cfg " + "crabjob_" + channels[i] + ".cfg"
+    submitcommand = "crab -create -cfg " + physMode + "cb_" + channels[i] + ".cfg"
     child   = os.system(submitcommand)
     child2   = os.system("crab -submit")
--- test/submitToCrabData-Wmunu-CertifiedJSON.py
+++ test/submitToCrabData-Wmunu-CertifiedJSON.py
@@ -2,80 +2,105 @@ import os,sys
 import string, re
 from time import gmtime, localtime, strftime
 
-##------ Please set ONLY one of the four flags to True -------
 physMode   = "WmunuJets_"
 ConfigFile = "WmunuJetsAnalysis_cfg.py"
+DefTrig    = "'HLT_Mu9','HLT_Mu11','HLT_Mu13','HLT_Mu15_v*','HLT_Mu17_v*','HLT_Mu24_v*','HLT_Mu30_v*','HLT_IsoMu17_v*','HLT_IsoMu24_v*','HLT_IsoMu30_v*'"
 
-## ----- 2010A
-#dataset    =     ["/Mu/Run2010A-Apr21ReReco-v1/AOD"]
-#channels   =         ["Run2010A-Apr21ReReco-v1"]
-#outputdir  = "CMSSW428-Run2010A-Apr21ReReco-v1"
-#RunRange   =     ["136033-149442"]
-#JSON       = "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt"
+dataset    = [
+    "/Mu/Run2010A-Apr21ReReco-v1/AOD",
+    "/Mu/Run2010B-Apr21ReReco-v1/AOD",
+    "/SingleMu/Run2011A-May10ReReco-v1/AOD",
+    "/SingleMu/Run2011A-PromptReco-v4/AOD",
+    "/SingleMu/Run2011A-PromptReco-v4/AOD",
+    "/SingleMu/Run2011A-05Aug2011-v1/AOD",
+    "/SingleMu/Run2011A-05Aug2011-v1/AOD",
+    "/SingleMu/Run2011A-PromptReco-v6/AOD",
+    "/SingleMu/Run2011A-PromptReco-v6/AOD",
+    "/SingleMu/Run2011B-PromptReco-v1/AOD",
+    "/SingleMu/Run2011B-PromptReco-v1/AOD",
+    "/SingleMu/Run2011B-PromptReco-v1/AOD",
+    "/SingleMu/Run2011B-PromptReco-v1/AOD"]
+channels   = [
+    "Mu-Run2010A-Apr21ReReco-v1-trigv2010",
+    "Mu-Run2010B-Apr21ReReco-v1-trigv2010",
+    "SingleMu-Run2011A-May10ReReco-v1-trig5E32",
+    "SingleMu-Run2011A-PromptReco-v4-trig1E33",
+    "SingleMu-Run2011A-PromptReco-v4-trig1p4E33",
+    "SingleMu-Run2011A-05Aug2011-v1-trigv2E33v11",
+    "SingleMu-Run2011A-05Aug2011-v1-trigv2E33v12",
+    "SingleMu-Run2011A-PromptReco-v6-trigv2E33v12",
+    "SingleMu-Run2011A-PromptReco-v6-trigv3E33v00",
+    "SingleMu-Run2011B-PromptReco-v1-trigv3E33v20",
+    "SingleMu-Run2011B-PromptReco-v1-trigv3E33v23",
+    "SingleMu-Run2011B-PromptReco-v1-trigv5E33v14",
+    "SingleMu-Run2011B-PromptReco-v1-trigv5E33v22"]
+trigname   = [
+    "'HLT_Mu9','HLT_Mu11','HLT_Mu13','HLT_Mu15_v*','HLT_Mu17_v*','HLT_Mu24_v*'",
+    "'HLT_Mu9','HLT_Mu11','HLT_Mu13','HLT_Mu15_v*','HLT_Mu17_v*','HLT_Mu24_v*'",
+    "'HLT_IsoMu17_v*','HLT_Mu30_v*'",
+    "'HLT_IsoMu17_v*','HLT_Mu30_v*'",
+    "'HLT_IsoMu17_v*','HLT_Mu30_v*'",
+    "'HLT_IsoMu17_v13','HLT_IsoMu20_v8','HLT_IsoMu24_v8','HLT_Mu30_v7','HLT_Mu40_v5'",
+    "'HLT_IsoMu17_v13','HLT_IsoMu20_v8','HLT_IsoMu24_v8','HLT_Mu30_v7','HLT_Mu40_v5'",
+    "'HLT_IsoMu17_v13','HLT_IsoMu20_v8','HLT_IsoMu24_v8','HLT_Mu30_v7','HLT_Mu40_v5'",
+    "'HLT_IsoMu20_v9','HLT_Mu40_eta2p1_v1'",
+    "'HLT_IsoMu30_eta2p1_v3','HLT_IsoMu24_eta2p1_v3','HLT_IsoMu24_v9','HLT_IsoMu20_v9','HLT_Mu40_eta2p1_v1','HLT_Mu40_v6'",
+    "'HLT_IsoMu30_eta2p1_v3','HLT_IsoMu24_eta2p1_v3','HLT_IsoMu24_v9','HLT_IsoMu20_v9','HLT_Mu40_eta2p1_v1','HLT_Mu40_v6'",
+    "'HLT_IsoMu30_eta2p1_v6','HLT_IsoMu24_eta2p1_v6','HLT_IsoMu24_v12','HLT_IsoMu30_eta2p1_v7','HLT_IsoMu24_eta2p1_v7','HLT_IsoMu24_v13','HLT_Mu40_eta2p1_v4','HLT_Mu40_v9'",
+    "'HLT_IsoMu30_eta2p1_v6','HLT_IsoMu24_eta2p1_v6','HLT_IsoMu24_v12','HLT_IsoMu30_eta2p1_v7','HLT_IsoMu24_eta2p1_v7','HLT_IsoMu24_v13','HLT_Mu40_eta2p1_v5','HLT_Mu40_v10'"]
+RunRange   = [
+    "136033-149442",
+    "136033-149442",
+    "160404-163869",
+    "165088-167038",
+    "167039-167913",
+    "170249-170825",
+    "170826-172619",
+    "172620-173235",
+    "173236-173692",
+    "175832-176460",
+    "176461-178419",
+    "178420-179958",
+    "179959-180252"]
+JSON       = [
+    "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt",
+    "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt",
+    "Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt",
+    "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt"]
+condor     = [1,1,1,1,1,1,1,1,1,1,1,1,1] # Total jobs 13 now
 
-
-## ----- 2010B
-#dataset    =     ["/Mu/Run2010B-Apr21ReReco-v1/AOD"]
-#channels   =         ["Run2010B-Apr21ReReco-v1"]
-#outputdir  = "CMSSW428-Run2010B-Apr21ReReco-v1"
-#RunRange   =     ["136033-149442"]
-#JSON       = "Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON.txt" 
-
-
-## ----- 2011A May10ReReco
-#dataset    = ["/SingleMu/Run2011A-May10ReReco-v1/AOD"]
-#channels   =           ["Run2011A-May10ReReco-v1"]
-#outputdir  =   "CMSSW428-Run2011A-May10ReReco-v1"
-#RunRange   =     ["160404-163869"]
-#JSON       = "Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt" 
-
-
-## ----- 2011A PromptReco-v4
-#dataset    = ["/SingleMu/Run2011A-PromptReco-v4/AOD"]
-#channels   =           ["Run2011A-PromptReco-v4"]
-#outputdir  =   "CMSSW428-Run2011A-PromptReco-v4"
-#RunRange   =     ["165071-168437"]
-#JSON       = "Cert_160404-173692_7TeV_PromptReco_Collisions11_JSON.txt" 
-
-
-## ----- 2011A Run2011A-05Aug2011-v1
-#dataset    = ["/SingleMu/Run2011A-05Aug2011-v1/AOD"]
-#channels   =           ["Run2011A-05Aug2011-v1"]
-#outputdir  =   "CMSSW428-Run2011A-05Aug2011-v1"
-#RunRange   =     ["170249-172619"]
-#JSON       = "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt" 
-
-
-# ----- 2011A Run2011A-PromptReco-v6
-dataset    = ["/SingleMu/Run2011A-PromptReco-v6/AOD"]
-channels   =           ["Run2011A-PromptReco-v6"]
-outputdir  =   "CMSSW428-Run2011A-PromptReco-v6"
-RunRange   =     ["172620-175770"]
-JSON       = "Cert_160404-173692_7TeV_PromptReco_Collisions11_JSON.txt" 
-
+MyResilientArea = "/yangf/CMSSW428/" +physMode
 
 ## ------------------------------------------
-condor     = [1]
-MyResilientArea = "/yangf/CMSSW428/" +physMode + outputdir
-## ------------------------------------------
 
 
-def changeMainConfigFile(trigpath):
+def changeMainConfigFile(trigpath,nowtrigname):
     fin  = open(ConfigFile)
-    pset_cfg      = "py_" + trigpath + ".py"
+    pset_cfg      = physMode + "py_" + trigpath + ".py"
     outfile_root  = physMode + trigpath + ".root"
     fout = open(pset_cfg,"w")
     for line in fin.readlines(): 
         if  line.find("demo.root")!=-1:
             line=line.replace("demo.root",outfile_root)
+        if  line.find(DefTrig)!=-1:
+            line=line.replace(DefTrig,nowtrigname)
         fout.write(line)
     print pset_cfg + " has been written.\n"
 
 
-def changeCrabTemplateFile(outfile, index):
+def changeCrabTemplateFile(outfile, index, nowJSON, nowRang):
     fin  = open("crabTemplate.cfg")
-    pset_cfg      = "py_" + outfile + ".py"
-    pset_crab     = "crabjob_" + outfile + ".cfg"
+    pset_cfg      = physMode + "py_" + outfile + ".py"
+    pset_crab     = physMode + "cb_" + outfile + ".cfg"
     outfile_root  = physMode + outfile + ".root"
     fout = open(pset_crab,"w")
     for line in fin.readlines():
@@ -83,13 +108,13 @@ def changeCrabTemplateFile(outfile, index):
             line=line.replace("mydataset",dataset[index])
             fout.write("\n")
             fout.write("runselection="+RunRange[index]+"\n")
-            fout.write("lumi_mask="+JSON+"\n")
+            fout.write("lumi_mask="+nowJSON+"\n")
         if line.find("myanalysis")!=-1:
             line=line.replace("myanalysis",pset_cfg)    
         if  line.find("myrootfile")!=-1:
             line=line.replace("myrootfile",outfile_root)
         if  line.find("myresilient")!=-1:
-            line=line.replace("myresilient",MyResilientArea)    
+            line=line.replace("myresilient",MyResilientArea+nowRang+"_"+outfile)    
         if line.find("glite")!=-1 and condor[index]!=0:
             line=line.replace("glite", "condor")        
         fout.write(line)        
@@ -98,15 +123,14 @@ def changeCrabTemplateFile(outfile, index):
       
     print pset_crab + " has been written.\n"
 
-
     
 ###################
 for i in range(len(channels)):
-    changeMainConfigFile(channels[i])
-    changeCrabTemplateFile(channels[i],i)
+    changeMainConfigFile(channels[i],trigname[i])
+    changeCrabTemplateFile(channels[i],i,JSON[i],RunRange[i])
 
 for i in range(len(channels)):
     #if i<9: continue
-    submitcommand = "crab -create -cfg " + "crabjob_" + channels[i] + ".cfg"
+    submitcommand = "crab -create -cfg " + physMode + "cb_" + channels[i] + ".cfg"
     child   = os.system(submitcommand)
     child2   = os.system("crab -submit")
