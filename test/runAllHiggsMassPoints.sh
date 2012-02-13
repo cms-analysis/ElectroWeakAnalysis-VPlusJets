4b7161aca441642834a90f59143377631f6d1174
Fan Yang <sha1-89248e455302f8b21edd4e6b7025364acd9760a8@cern.ch>
2012-02-13 22:31:21 +0000

    config

--- test/runAllHiggsMassPoints.sh
+++ test/runAllHiggsMassPoints.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 for i in 170 180 190 200 250 300 350 400 450 500 550 600
 do
-   echo "Now running Higgs mass point $i muons 2 jet"
-   python runHWWFitter.py -m HWWMuonsConfig -j 2 -H $i
-   echo "Now running Higgs mass point $i muons 3 jet"  
-   python runHWWFitter.py -m HWWMuonsConfig -j 3 -H $i
+   #echo "Now running Higgs mass point $i muons 2 jet"
+   #python runHWWFitter.py -m HWWMuonsConfig -j 2 -H $i
+   #echo "Now running Higgs mass point $i muons 3 jet"  
+   #python runHWWFitter.py -m HWWMuonsConfig -j 3 -H $i
    echo "Now running Higgs mass point $i electrons 2 jet"
    python runHWWFitter.py -m HWWElectronsConfig -j 2 -H $i
-   echo "Now running Higgs mass point $i electrons 3 jet"  
-   python runHWWFitter.py -m HWWElectronsConfig -j 3 -H $i
+   #echo "Now running Higgs mass point $i electrons 3 jet"  
+   #python runHWWFitter.py -m HWWElectronsConfig -j 3 -H $i
 done
