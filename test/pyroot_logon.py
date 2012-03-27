f1fc4ff185596d91898c0b644a030749985633c6
Jake Anderson <sha1-056215a7deed826ca8e627b453e96d110263ed4b@cern.ch>
2012-03-27 19:38:12 +0000

    add plots

--- test/pyroot_logon.py
+++ test/pyroot_logon.py
@@ -11,7 +11,7 @@ def cmsLabel(canvas, lumi, prelim = False, lumiLabel = 'fb'):
     canvas.cd()
     prelimText = ''
     if prelim:
-        prelimText = ' Preliminary'
+        prelimText = ' preliminary'
     l.DrawLatex(1. - canvas.GetRightMargin(), 1. - canvas.GetTopMargin() + 0.01,
                 'CMS%s, #scale[0.5]{#lower[-0.15]{#it{#int}}}#it{L} dt = %0.1f#kern[0.2]{%s}^{-1}, #sqrt{#it{s}} = 7#kern[0.1]{TeV}' % \
                 (prelimText, lumi, lumiLabel)
