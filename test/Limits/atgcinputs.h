b501de8fc15bf60aea848828bcc792c8e35614f5
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2012-05-18 02:42:43 +0000

    add cut-and-count capability, adjust to new inputs prior to freezing

--- test/Limits/atgcinputs.h
+++ test/Limits/atgcinputs.h
@@ -25,13 +25,16 @@ const double siglepteffunc  = 0.02;
 const double siglumiunc     = 0.022;
 const double signal_xs_unc  = 0.034;
 
-const float LAMBDAZ_MIN = -0.6;
-const float LAMBDAZ_MAX =  0.6;
-const float LAMBDAZ_INC = 0.2;
+// for cut-and-count limits:
+const double dijetptmingev  = 200.;
+
+const float LAMBDAZ_MIN = -0.18;
+const float LAMBDAZ_MAX =  0.18;
+const float LAMBDAZ_INC = 0.01;
 
 const float dKG_MIN = -0.6;
 const float dKG_MAX =  0.6;
-const float dKG_INC = 0.2;
+const float dKG_INC = 0.1;
 
 // FILE/HISTO STRUCTURE: assumed same name objects contained in different files for the different inputs
 
@@ -40,11 +43,13 @@ const char *dir = "/uscms_data/d2/kalanand/junk/vplusjets/CMSSW_4_2_8/src/Electr
 // ordered the same as the inputs
 //
 const char *inputfiles[NUMCHAN] = {
-  "el_btag.root",
-  "el_noBtag.root",
-  "mu_btag.root",
-  "mu_noBtag.root"
+  "new_el_btag.root",
+  "new_el_noBtag.root",
+  "new_mu_btag.root",
+  "new_mu_noBtag.root"
 };
+
+const char *signalfmtstr = "lambdaZ_%.2f_deltaKappaGamma_%.2f";
 
 const char *dataobjname = "data";
 const char *bkgdobjname = "background";
