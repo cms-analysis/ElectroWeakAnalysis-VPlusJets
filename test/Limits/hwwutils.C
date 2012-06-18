54f581153c5ebbfb336fce0452cd567aa63153c7
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2012-06-18 19:17:54 +0000

    add jet bin error utility routines

--- test/Limits/hwwutils.C
+++ test/Limits/hwwutils.C
@@ -24,6 +24,13 @@ struct HdataPerMassPt {
   double vbfscaleunclo;
   double br2lnujj;
   double br2ww;
+
+  double qcdscale_ggh0j;
+  double qcdscale_ggh1in0j;
+  double qcdscale_ggh1in1j;
+  double qcdscale_ggh2in1j;
+  double ueps_0j;
+  double ueps_1j;
 };
 
 std::map<int,HdataPerMassPt> m_signals; // signal (Higgs) data per mass point
@@ -100,6 +107,43 @@ void readBRtable(const std::string& fname)
 
 //================================================================================
 
+void readJetBinErrTable(const std::string& fname)
+{
+  cout << "Reading exclusive jet bin theoretical nuisances from " << fname << endl;
+
+  FILE *brf = fopen(fname.c_str(),"r");
+  if (!brf) {
+    cerr << "Couldn't open " << fname << endl;
+    exit(-1);
+  }
+
+  char str[128];
+  while (!feof(brf) && fgets(str,128,brf)) {
+    if (str[0] == '#') continue;
+    int mHgev;
+    char param[25], val0j[20], val1j[20];
+    int n = sscanf(str,"%d %s %s %s", &mHgev, param, val0j, val1j);
+    if (n != 4) {
+      cerr << "Error reading from file " << fname << endl;
+      exit(-1);
+    }
+    if (!strcmp(param,"UEPS")) {
+      m_signals[mHgev].ueps_0j = strtod(val0j,NULL);
+      m_signals[mHgev].ueps_1j = strtod(val1j,NULL);
+    } else if (!strcmp(param,"QCDscale_ggH1in")) {
+      m_signals[mHgev].qcdscale_ggh1in0j = strtod(val0j,NULL);
+      m_signals[mHgev].qcdscale_ggh1in1j = strtod(val1j,NULL);
+    } else if (!strcmp(param,"QCDscale_ggH2in")) {
+      m_signals[mHgev].qcdscale_ggh2in1j = strtod(val1j,NULL);
+    } else if (!strcmp(param,"QCDscale_ggH")) {
+      m_signals[mHgev].qcdscale_ggh0j = strtod(val0j,NULL);
+    }
+  }
+  fclose(brf);
+}
+
+//================================================================================
+
 int findMasspt2use(int massgev)
 {
   // NB: the scheme of which mass to use for uncertainties must be coordinated manually
@@ -127,17 +171,29 @@ int findMasspt2use(int massgev)
 }
 
 //================================================================================
+const std::pair<double,double> zeropair(0.,0.);
 
 void makeTheoretUncert4Sig(int massgev,
 			   const TString& procname,
 			   pair<double,double>& pdfunc,   // down/up pair to put in card
-			   pair<double,double>& scaleunc) // down/up pair to put in card
+			   pair<double,double>& scaleunc0, // down/up pair to put in card
+			   pair<double,double>& scaleunc1,
+			   pair<double,double>& scaleunc2,
+			   pair<double,double>& scaleunc3,
+			   pair<double,double>& ueps0j,
+			   pair<double,double>& ueps1j)
 {
   std::map<int,HdataPerMassPt>::const_iterator it =  m_signals.find(massgev);
   if (it == m_signals.end()) {
     cerr << "Mass " << massgev << "GeV not represented in signal tables." << endl;
     exit(-1);
   }
+
+  scaleunc1 = zeropair;
+  scaleunc2 = zeropair;
+  scaleunc3 = zeropair;
+  ueps0j    = zeropair;
+  ueps1j    = zeropair;
     
   const HdataPerMassPt& hd = it->second;
 
@@ -148,16 +204,16 @@ void makeTheoretUncert4Sig(int massgev,
   if (procname.Contains("qq") ) { // vbf process
 #if 0
     // this version gives two sided inputs to combine
-    pdfunc.first    = 1.0 + (hd.vbfpdfunclo/100.0);
-    pdfunc.second   = 1.0 + (hd.vbfpdfunchi/100.0);
-    scaleunc.first  = 1.0 + (hd.vbfscaleunclo/100.0);
-    scaleunc.second = 1.0 + (hd.vbfscaleunchi/100.0);
+    pdfunc.first     = 1.0 + (hd.vbfpdfunclo/100.0);
+    pdfunc.second    = 1.0 + (hd.vbfpdfunchi/100.0);
+    scaleunc0.first  = 1.0 + (hd.vbfscaleunclo/100.0);
+    scaleunc0.second = 1.0 + (hd.vbfscaleunchi/100.0);
 #else
     // this version takes the geometric mean of low and high to feed as single number
-    pdfunc.first    = 0.0;
-    pdfunc.second   = sqrt( (1.0+(hd.vbfpdfunchi/100.))/(1.0+(hd.vbfpdfunclo/100.)) );
-    scaleunc.first  = 0.0;
-    scaleunc.second = sqrt( (1.0+(hd.vbfscaleunchi/100.))/(1.0+(hd.vbfscaleunclo/100.)) );
+    pdfunc.first     = 0.0;
+    pdfunc.second    = sqrt( (1.0+(hd.vbfpdfunchi/100.))/(1.0+(hd.vbfpdfunclo/100.)) );
+    scaleunc0.first  = 0.0;
+    scaleunc0.second = sqrt( (1.0+(hd.vbfscaleunchi/100.))/(1.0+(hd.vbfscaleunclo/100.)) );
 #endif
     // add signal acceptance effect of PDF variation in quadrature:
     pdfunc.second = 1.0 + sqrt((pdfunc.second-1)*(pdfunc.second-1) +
@@ -165,16 +221,19 @@ void makeTheoretUncert4Sig(int massgev,
   } else { // default gg fusion process
 #if 0
     // this version gives two sided inputs to combine
-    pdfunc.first    = 1.0 + (hd.ggHpdfunclo/100.0);
-    pdfunc.second   = 1.0 + (hd.ggHpdfunchi/100.0);
-    scaleunc.first  = 1.0 + (hd.ggHscaleunclo/100.0);
-    scaleunc.second = 1.0 + (hd.ggHscaleunchi/100.0);
+    pdfunc.first     = 1.0 + (hd.ggHpdfunclo/100.0);
+    pdfunc.second    = 1.0 + (hd.ggHpdfunchi/100.0);
+    scaleunc0.first  = 1.0 + (hd.ggHscaleunclo/100.0);
+    scaleunc0.second = 1.0 + (hd.ggHscaleunchi/100.0);
 #else
     // this version takes the geometric mean of low and high to feed as single number
-    pdfunc.first    = 0.0;
-    pdfunc.second   = sqrt( (1.0+(hd.ggHpdfunchi/100.))/(1.0+(hd.ggHpdfunclo/100.)) );
-    scaleunc.first  = 0.0;
-    scaleunc.second = sqrt( (1.0+(hd.ggHscaleunchi/100.))/(1.0+(hd.ggHscaleunclo/100.)) );
+    pdfunc    = std::pair<double,double>(0.0, sqrt( (1.0+(hd.ggHpdfunchi/100.))/(1.0+(hd.ggHpdfunclo/100.))));
+    scaleunc0 = std::pair<double,double>(0.0, hd.qcdscale_ggh0j);
+    scaleunc1 = std::pair<double,double>(0.0, hd.qcdscale_ggh1in0j);
+    scaleunc2 = std::pair<double,double>(0.0, hd.qcdscale_ggh1in1j);
+    scaleunc3 = std::pair<double,double>(0.0, hd.qcdscale_ggh2in1j);
+    ueps0j    = std::pair<double,double>(0.0, hd.ueps_0j);
+    ueps1j    = std::pair<double,double>(0.0, hd.ueps_1j);
 #endif
     // add signal acceptance effect of PDF variation in quadrature:
     pdfunc.second = 1.0 + sqrt((pdfunc.second-1)*(pdfunc.second-1) +
