a28185ebea9eeafad810359b8a113768a78813f8
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2012-11-28 21:10:34 +0000

    add BEAME, usage info

--- test/Limits/doitall.sh
+++ test/Limits/doitall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+BEAME=8TeV
+#BEAME=7TeV
 #PREFIX=hww-histo-shapes
-PREFIX=hwwlvjj.input_8TeV
+PREFIX=hwwlvjj.input_${BEAME}
 
 if [ $# -lt 1 ]
 then
@@ -16,6 +18,13 @@ then
     DIR=.
 else
     DIR=$2
+fi
+
+if [ ! -d $DIR ]
+then
+    echo "No dir $DIR"
+    echo "Usage: $0 nametag [workingdir] [inhibit datacard-making] [inhibit combine] [inhibit parsing, just plot]"
+    exit
 fi
 
 # add any argument beyond 2 to inhibit datacard making
@@ -34,10 +43,10 @@ fi
 # add any argument beyond 3 to inhibit datacard making and running combine...
 if [ $# -lt 4 ] 
 then
-if stat -t ${DIR}/datacard_8TeV-${1}[-_]M=???.txt >/dev/null 2>&1
+if stat -t ${DIR}/datacard_${BEAME}-${1}[-_]M=???.txt >/dev/null 2>&1
 then
     echo "Running combine"
-    ./combine_asymp.sh ${DIR}/datacard_8TeV-${1}[-_]M=???.txt
+    ./combine_asymp.sh ${DIR}/datacard_${BEAME}-${1}[-_]M=???.txt
 fi
 fi
 #
@@ -45,15 +54,15 @@ fi
 # (just plot)
 if [ $# -lt 5 ] 
 then
-if stat -t  ${DIR}/limit_8TeV-${1}[-_]M=???.log >/dev/null 2>&1
+if stat -t  ${DIR}/limit_${BEAME}-${1}[-_]M=???.log >/dev/null 2>&1
 then
     echo "Parsing output"
-    ./parseAsym.sh ${DIR}/limit_8TeV-${1}[-_]M=???.log
+    ./parseAsym.sh ${DIR}/limit_${BEAME}-${1}[-_]M=???.log
 fi
 fi
 #
-if [ -e ${DIR}/limit_8TeV-$1.tab ]
+if [ -e ${DIR}/limit_${BEAME}-$1.tab ]
 then
     echo "plotting limit"
-    root -n -b -q "plotLimit.C(\"${DIR}/limit_8TeV-$1.tab\")"
+    root -n -b -q "plotLimit.C(\"${DIR}/limit_${BEAME}-$1.tab\")"
 fi
