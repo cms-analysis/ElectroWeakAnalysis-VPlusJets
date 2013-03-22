d0af00518d0a5c69b0ba862a7cdfc0b12d228724
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2013-03-22 19:34:26 +0000

    tentative 2012 inputs

--- test/Limits/atgcinputs.h
+++ test/Limits/atgcinputs.h
@@ -10,8 +10,10 @@ const int masspts[NUMMASSPTS] = { 150 };
 #define NUMCHAN 2
 
 const char *channames[NUMCHAN] = {
-  "elnobtag",
-  "munobtag",
+  //  "elboosted",
+  "eldijet",
+  //  "muboosted",
+  "mudijet",
 };
 
 const TString AnalStr       = "ATGC";
@@ -20,19 +22,23 @@ const double mutrigeff      = 1.;
 const double eltrigeff      = 1.;
 const double sigtrigeffunc  = 0.01;
 const double siglepteffunc  = 0.02;
-const double siglumiunc     = 0.022;
+const double siglumiunc     = 0.044;
 const double signal_xs_unc  = 0.034;
 
 // for cut-and-count limits:
 const double dijetptmingev  = 350.;
 
-const float LAMBDAZ_MIN = -0.15;
-const float LAMBDAZ_MAX =  0.15;
-const float LAMBDAZ_INC = 0.005;
+const float LAMBDAZ_MIN = -0.1;
+const float LAMBDAZ_MAX =  0.1;
+const float LAMBDAZ_INC = 0.005; //  41 pts
 
-const float dKG_MIN =  -0.40;
-const float dKG_MAX =  -0.30;
-const float dKG_INC = 0.01;
+const float dKG_MIN =  -0.30;
+const float dKG_MAX =   0.30;
+const float dKG_INC =   0.02;    // x31 pts
+
+const float dg1_MIN =  -0.60;
+const float dg1_MAX =   0.60;
+const float dg1_INC =   0.05;    // x25 pts = 31775 total pts
 
 // FILE/HISTO STRUCTURE: assumed same name objects contained in different files for the different inputs
 
@@ -42,8 +48,10 @@ const char *dir = ".";
 // ordered the same as the inputs
 //
 const char *inputfiles[NUMCHAN] = {
-  "el_noBtag.root",
-  "mu_noBtag.root"
+  //"el_boosted.root",
+  "el_dijet.root",
+  //"mu_boosted.root",
+  "mu_dijet.root",
 };
 
 const char *signalfmtstr = "lambdaZ_%.3f_deltaKappaGamma_%.3f";
