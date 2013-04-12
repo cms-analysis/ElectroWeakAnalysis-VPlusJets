309f472f870e2c87126dd8a841ab8a55fd7c6616
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2013-04-12 00:31:25 +0000

    tweaks to header line

--- test/Limits/parseAsym.sh
+++ test/Limits/parseAsym.sh
@@ -5,7 +5,7 @@ file=`basename $1 .log`
 suffix=${file##limit_}
 suffix=${suffix%%[-_]M=[0-9]*}
 
-echo "hhmass   obsLimit        exp95lo         exp68lo         expLimit        exp68hi         exp95hi" | tee ${DIR}/limit_${suffix}.tab
+echo "hmass    obs   exp95lo  exp68lo   exp  exp68hi  exp95hi" | tee ${DIR}/limit_${suffix}.tab
 
 for i in $*; do
     file=`basename $i .log`
