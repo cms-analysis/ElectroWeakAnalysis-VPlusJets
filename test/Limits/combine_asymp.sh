45f3bebc064f7e89f6ab4819935313dda458f57d
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2013-04-12 00:32:08 +0000

    tweaks

--- test/Limits/combine_asymp.sh
+++ test/Limits/combine_asymp.sh
@@ -6,7 +6,8 @@ then
     exit
 fi
 
-COMBINE_ARGS="-v0 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping --rMin=-5 --rMax=10"
+#COMBINE_ARGS="-v0 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping --rMin=0.1 --rMax=5"
+COMBINE_ARGS="-v4 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping"
 
 #for mass in 170 180 190 200 250 300 350 400 450 500 550 600
 #for mass in 180 250 400
@@ -16,6 +17,10 @@ do
   file=`basename $datacard .txt`
   suffix=${file##datacard_}
   mass=`ls $datacard | egrep -o "M=[0-9]+" | egrep -o "[0-9]+"`
+#
+# "tee" used per recommendation from Giovanni, since it captures the stdout/stderr better than straight redirection;
+# the combine tool mucks with the stdout FILE descriptor...
+#
   echo "combine $COMBINE_ARGS -m ${mass} $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null"
   combine $COMBINE_ARGS -m ${mass} $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null
 #  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_${mass}.txt 2>&1 | tee ${DIR}/limit_${mass}.log >/dev/null
