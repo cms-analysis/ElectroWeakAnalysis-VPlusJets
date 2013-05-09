5c1b4e2dc329f4559b88f1f93893e11b066b5ef6
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2013-05-09 21:54:35 +0000

    6_1_1, support for profile likelihood limits, dkg vs dg1 limits

--- test/Limits/atgccondormylimit.sh
+++ test/Limits/atgccondormylimit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SRCDIR=/uscms/home/pdudero/work/CMSSW_5_3_3_patch3/src/ElectroWeakAnalysis/VPlusJets/test/Limits/
-SRCDIR=/uscms/home/pdudero/work/CMSSW_6_1_1/src/Limits/
-executable=${SRCDIR}/combine_asymp_atgc_batch.sh
-cmbscript=${SRCDIR}/combine_asymp_atgc.sh
-#executable=${SRCDIR}/combine_fc_atgc_batch.sh
-#cmbscript=${SRCDIR}/combine_fc_atgc.sh
+SRCDIR=/uscms/home/pdudero/work/CMSSW_6_1_1/src/ElectroWeakAnalysis/VPlusJets/test/Limits
+executable=${SRCDIR}/batchit.sh
+#cmbscript1=${SRCDIR}/combine_plobs_atgc.sh
+#cmbscript2=${SRCDIR}/combine_plexp_atgc.sh
+cmbscript1=${SRCDIR}/combine_asymp_atgc.sh
+#cmbscript1=${SRCDIR}/combine_fc_atgc.sh
 
 if [ $# -lt 1 ]
 then
-    echo "Usage: $0 datacard*lz*.txt"
+    echo "Usage: $0 datacard*.txt"
     exit
 fi
 
@@ -27,16 +27,59 @@ EOF
 printheader
 
 lzlist=`ls $* | egrep -o "lz_[0-9\.\-]+" | sed 's#lz_##g'|sort -u`
-for lz in $lzlist
-do
-  arglist=`ls ${PWD}/datacard*_lz_${lz}*.txt|xargs`
-  xferlist=`echo $arglist|sed 's# #,#g'`
-  echo "transfer_input_files = $xferlist,el_dijet.root,el_boosted.root,mu_dijet.root,mu_boosted.root,${cmbscript}" >>$submit
-  echo "Error = atgclimit_lz${lz}.err" >>$submit
-  echo "Output = atgclimit_lz${lz}.out" >>$submit
-  echo "Arguments = $arglist" >>$submit
-  echo "Queue" >>$submit
-done
+echo "lzlist =" $lzlist
+if [[ "$lzlist" != "" ]]
+then
+    for lz in $lzlist
+    do
+      arglist=`ls $* | grep "datacard_lz_${lz}"|xargs`
+      xferlist=`echo $arglist|sed 's# #,#g'`
+  # echo "transfer_input_files = $xferlist,el_dijet.root,el_boosted.root,mu_dijet.root,mu_boosted.root" >>$submit
+      echo "transfer_input_files = $xferlist,el_boosted.root,mu_boosted.root" >>$submit
+      echo "Error = atgclimit_lz${lz}.err" >>$submit
+      echo "Output = atgclimit_lz${lz}.out" >>$submit
+      echo "Arguments = $cmbscript1 $arglist" >>$submit
+      echo "Queue" >>$submit
+      
+      if [[ "$cmbscript2" != "" ]]
+      then
+	  if [[ `echo "scale=4; $lz <= 0.003 && $lz >= -0.004" | bc` -ne 0 ]]
+	  then
+              # for these, allocate one file per job
+	      filelist=`ls $* | grep "_\$lz"`
+	      for file in $filelist
+              #for file in $*
+	      do
+		base=`basename $file .txt`
+		suffix=${basedatacard_}
+		echo "transfer_input_files = $file,el_boosted.root,mu_boosted.root" >>$submit
+		echo "Error = atgclimit_${suffix}_exp.err" >>$submit
+		echo "Output = atgclimit_${suffix}_exp.out" >>$submit
+		echo "Arguments = $cmbscript1 $file" >>$submit
+		echo "Queue" >>$submit
+	      done
+	  else
+	      echo "transfer_input_files = $xferlist,el_boosted.root,mu_boosted.root" >>$submit
+	      echo "Error = atgclimit_lz${lz}_exp.err" >>$submit
+	      echo "Output = atgclimit_lz${lz}_exp.out" >>$submit
+	      echo "Arguments = $cmbscript2 $arglist" >>$submit
+	      echo "Queue" >>$submit
+	  fi
+      fi
+    done
+else
+    for file in $*
+    do
+      base=`basename $file .txt`
+      suffix=${basedatacard_}
+      echo "transfer_input_files = $file,el_boosted.root,mu_boosted.root" >>$submit
+      echo "Error = atgclimit_${suffix}_exp.err" >>$submit
+      echo "Output = atgclimit_${suffix}_exp.out" >>$submit
+      echo "Arguments = $cmbscript1 $file" >>$submit
+      echo "Queue" >>$submit
+    done
+fi
+
 
 # for dc in $*
 # do
