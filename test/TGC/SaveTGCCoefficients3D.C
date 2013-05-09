17408e4e1ad877b6fe7a51a953d5466026a48b34
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2013-05-09 23:49:27 +0000

    decrease ranges, increase resolution, add dkg:dg1

--- test/TGC/SaveTGCCoefficients3D.C
+++ test/TGC/SaveTGCCoefficients3D.C
@@ -5,42 +5,42 @@ void SaveTGCCoefficients3D(){
    ////// ---- first process lambda:dkappa combination --------
    TFile fin("root_kappa-lambda.root");
    TTree* tr = (TTree*) fin.Get("tree"); 
-   TProfile2D* p0_lambda_dk = new TProfile2D("p0_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55);
-   SetTitles(p0_lambda_dk, "lambda", "dkappa", "p0");
-   TProfile2D* p1_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p1_lambda_dk");
-   TProfile2D* p2_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p2_lambda_dk");
-   TProfile2D* p3_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p3_lambda_dk");
-   TProfile2D* p4_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p4_lambda_dk");
-   TProfile2D* p5_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p5_lambda_dk");
-   TProfile2D* p6_lambda_dk = (TProfile2D*) p0_lambda_dk->Clone("p6_lambda_dk");
+   TProfile2D* p0_lambda_dkg = new TProfile2D("p0_lambda_dkg","", 61, -0.0305, 0.0305, 21, -0.105, 0.105);
+   SetTitles(p0_lambda_dkg, "lambda", "dkg", "p0");
+   TProfile2D* p1_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p1_lambda_dkg");
+   TProfile2D* p2_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p2_lambda_dkg");
+   TProfile2D* p3_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p3_lambda_dkg");
+   TProfile2D* p4_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p4_lambda_dkg");
+   TProfile2D* p5_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p5_lambda_dkg");
+   TProfile2D* p6_lambda_dkg = (TProfile2D*) p0_lambda_dkg->Clone("p6_lambda_dkg");
 
-//    TProfile2D p1_lambda_dk("p1_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -0.003, 0.0003);
-//    SetTitles(p1_lambda_dk, "lambda", "dkappa", "p1");
-//    TProfile2D p2_lambda_dk("p2_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -3.0e-06, 0.2e-03);
-//    SetTitles(p2_lambda_dk, "lambda", "dkappa", "p2");
-//    TProfile2D p3_lambda_dk("p3_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -2.0e-06, 0.1e-06);
-//    SetTitles(p3_lambda_dk, "lambda", "dkappa", "p3");
-//    TProfile2D p4_lambda_dk("p4_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -1.0e-09, 25.0e-09);
-//    SetTitles(p4_lambda_dk, "lambda", "dkappa", "p4");
-//    TProfile2D p5_lambda_dk("p5_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -26.0e-12, 1.0e-12);
-//    SetTitles(p5_lambda_dk, "lambda", "dkappa", "p5");
-//    TProfile2D p6_lambda_dk("p6_lambda_dk","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -4.0e-15, 13.0e-15);
-//    SetTitles(p6_lambda_dk, "lambda", "dkappa", "p6");
+//    TProfile2D p1_lambda_dkg("p1_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -0.003, 0.0003);
+//    SetTitles(p1_lambda_dkg, "lambda", "dkg", "p1");
+//    TProfile2D p2_lambda_dkg("p2_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -3.0e-06, 0.2e-03);
+//    SetTitles(p2_lambda_dkg, "lambda", "dkg", "p2");
+//    TProfile2D p3_lambda_dkg("p3_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -2.0e-06, 0.1e-06);
+//    SetTitles(p3_lambda_dkg, "lambda", "dkg", "p3");
+//    TProfile2D p4_lambda_dkg("p4_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -1.0e-09, 25.0e-09);
+//    SetTitles(p4_lambda_dkg, "lambda", "dkg", "p4");
+//    TProfile2D p5_lambda_dkg("p5_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -26.0e-12, 1.0e-12);
+//    SetTitles(p5_lambda_dkg, "lambda", "dkg", "p5");
+//    TProfile2D p6_lambda_dkg("p6_lambda_dkg","", 55, -0.55, 0.55, 55, -0.55, 0.55, 100, -4.0e-15, 13.0e-15);
+//    SetTitles(p6_lambda_dkg, "lambda", "dkg", "p6");
 
-   tr->Draw("p0:lambda:dkappa>>p0_lambda_dk","","goff");
-   tr->Draw("p1:lambda:dkappa>>p1_lambda_dk","","goff");
-   tr->Draw("p2:lambda:dkappa>>p2_lambda_dk","","goff");
-   tr->Draw("p3:lambda:dkappa>>p3_lambda_dk","","goff");
-   tr->Draw("p4:lambda:dkappa>>p4_lambda_dk","","goff");
-   tr->Draw("p5:lambda:dkappa>>p5_lambda_dk","","goff");
-   tr->Draw("p6:lambda:dkappa>>p6_lambda_dk","","goff");
+   tr->Draw("p0:lambda:dkappa>>p0_lambda_dkg","","goff");
+   tr->Draw("p1:lambda:dkappa>>p1_lambda_dkg","","goff");
+   tr->Draw("p2:lambda:dkappa>>p2_lambda_dkg","","goff");
+   tr->Draw("p3:lambda:dkappa>>p3_lambda_dkg","","goff");
+   tr->Draw("p4:lambda:dkappa>>p4_lambda_dkg","","goff");
+   tr->Draw("p5:lambda:dkappa>>p5_lambda_dkg","","goff");
+   tr->Draw("p6:lambda:dkappa>>p6_lambda_dkg","","goff");
 
 
 
-   ////// ---- then process lambda:dg1 combination --------
+   ////// ---- second process lambda:dg1 combination --------
    TFile fin2("root_lambda-g1.root");
    TTree* tr2 = (TTree*) fin2.Get("tree"); 
-   TProfile2D* p0_lambda_dg1 = new TProfile2D("p0_lambda_dg1","", 55, -0.55, 0.55, 55, -0.55, 0.55);
+   TProfile2D* p0_lambda_dg1 = new TProfile2D("p0_lambda_dg1","", 61, -0.0305, 0.0305, 21, -0.105, 0.105);
    SetTitles(p0_lambda_dg1, "lambda", "dg1", "p0");
    TProfile2D* p1_lambda_dg1 = (TProfile2D*) p0_lambda_dg1->Clone("p1_lambda_dg1");
    TProfile2D* p2_lambda_dg1 = (TProfile2D*) p0_lambda_dg1->Clone("p2_lambda_dg1");
@@ -71,16 +71,37 @@ void SaveTGCCoefficients3D(){
    tr2->Draw("p6:dg1:lambda>>p6_lambda_dg1","","goff");
 
 
+   ////// ---- third process dkg:dg1 combination --------
+   TFile fin3("root_kappa-g1.root");
+   TTree* tr3 = (TTree*) fin3.Get("tree"); 
+   TProfile2D* p0_dkg_dg1 = new TProfile2D("p0_dkg_dg1","", 21, -0.105, 0.105, 21, -0.105, 0.105);
+   SetTitles(p0_dkg_dg1, "dkg", "dg1", "p0");
+   TProfile2D* p1_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p1_dkg_dg1");
+   TProfile2D* p2_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p2_dkg_dg1");
+   TProfile2D* p3_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p3_dkg_dg1");
+   TProfile2D* p4_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p4_dkg_dg1");
+   TProfile2D* p5_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p5_dkg_dg1");
+   TProfile2D* p6_dkg_dg1 = (TProfile2D*) p0_dkg_dg1->Clone("p6_dkg_dg1");
+
+   tr3->Draw("p0:dg1:dkappa>>p0_dkg_dg1","","goff");
+   tr3->Draw("p1:dg1:dkappa>>p1_dkg_dg1","","goff");
+   tr3->Draw("p2:dg1:dkappa>>p2_dkg_dg1","","goff");
+   tr3->Draw("p3:dg1:dkappa>>p3_dkg_dg1","","goff");
+   tr3->Draw("p4:dg1:dkappa>>p4_dkg_dg1","","goff");
+   tr3->Draw("p5:dg1:dkappa>>p5_dkg_dg1","","goff");
+   tr3->Draw("p6:dg1:dkappa>>p6_dkg_dg1","","goff");
+
+
 
    /////----- Let's save all histograms in the output file ----
    fout.cd();
-   p0_lambda_dk->Write();
-   p1_lambda_dk->Write();
-   p2_lambda_dk->Write();
-   p3_lambda_dk->Write();
-   p4_lambda_dk->Write();
-   p5_lambda_dk->Write();
-   p6_lambda_dk->Write();
+   p0_lambda_dkg->Write();
+   p1_lambda_dkg->Write();
+   p2_lambda_dkg->Write();
+   p3_lambda_dkg->Write();
+   p4_lambda_dkg->Write();
+   p5_lambda_dkg->Write();
+   p6_lambda_dkg->Write();
 
    p0_lambda_dg1->Write();
    p1_lambda_dg1->Write();
@@ -89,6 +110,14 @@ void SaveTGCCoefficients3D(){
    p4_lambda_dg1->Write();
    p5_lambda_dg1->Write();
    p6_lambda_dg1->Write();
+
+   p0_dkg_dg1->Write();
+   p1_dkg_dg1->Write();
+   p2_dkg_dg1->Write();
+   p3_dkg_dg1->Write();
+   p4_dkg_dg1->Write();
+   p5_dkg_dg1->Write();
+   p6_dkg_dg1->Write();
 
    fout.Close();
 }
